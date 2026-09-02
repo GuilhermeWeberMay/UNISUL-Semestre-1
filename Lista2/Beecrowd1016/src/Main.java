0d7c88d0514adbac1562e69a7ab6ac31ea4e54af
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-09-02 11:34:59 -0300

    Desafio 1016 feito

---
+++ Lista2/Beecrowd1016/src/Main.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int km = sc.nextInt();
+        int distancia = km * 2;
+        System.out.println(distancia + " minutos");
+    }
+    
+}
