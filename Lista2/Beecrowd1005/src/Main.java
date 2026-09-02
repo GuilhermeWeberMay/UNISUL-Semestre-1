f78d27ebcda5464f0b615f03ad222c328c8e076b
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-09-02 09:27:19 -0300

    Desafio 1005 feito

---
+++ Lista2/Beecrowd1005/src/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double A, B, MEDIA;
+        A=input.nextDouble();
+        B=input.nextDouble();
+        MEDIA=(A*3.5+B*7.5)/(3.5+7.5);
+        System.out.printf("MEDIA = %.5f",MEDIA);
+        System.out.println();
+    }
+
+}
