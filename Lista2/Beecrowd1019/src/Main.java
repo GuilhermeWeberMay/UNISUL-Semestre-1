688d0fc2d69e90f617fc34668deabf78df2009d9
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-09-02 12:08:57 -0300

    Desafio 1019 feito

---
+++ Lista2/Beecrowd1019/src/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tempo = sc.nextInt();
+        int segundos = tempo % 60;
+        int minutos= (tempo % 3600) / 60;
+        int horas = tempo / 3600;
+        System.out.println(horas + ":" + minutos + ":" + segundos);
+        System.out.println();
+    }
+
+}
