6d250870331a8d06c50abfae7b7c4a27ddf95c57
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-09-02 10:46:44 -0300

    Desafio 1012 feito

---
+++ Lista2/Beecrowd1012/src/Main.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double pi = 3.14159;
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        double triangulo = a * c / 2;
+        double circulo = pi * (c * c);
+        double trapezio = (a + b) * c / 2;
+        double quadrado = b * b;
+        double retangulo = a * b;
+        System.out.printf("TRINANGULO = %.3f", triangulo);
+        System.out.println();
+        System.out.printf("CIRCULO = %.3f", circulo);
+        System.out.println();
+        System.out.printf("TRAPEZIO = %.3f", trapezio);
+        System.out.println();
+        System.out.printf("QUADRADO = %.3f", quadrado);
+        System.out.println();
+        System.out.printf("RETANGULO = %.3f", retangulo);
+        System.out.println();
+    }
+
+}
