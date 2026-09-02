6b803e26aec0ed70313fa8973413a25351bb3b0e
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-09-02 11:48:27 -0300

    Desafio 1018 feito

---
+++ Lista2/Beecrowd1018/src/Main.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int valor = sc.nextInt();
+
+        System.out.println(valor);
+
+        int notas100 = valor / 100;
+        valor = valor % 100;
+
+        int notas50 = valor / 50;
+        valor = valor % 50;
+
+        int notas20 = valor / 20;
+        valor = valor % 20;
+
+        int notas10 = valor / 10;
+        valor = valor % 10;
+
+        int notas5 = valor / 5;
+        valor = valor % 5;
+
+        int notas2 = valor / 2;
+        valor = valor % 2;
+
+        int notas1 = valor;
+
+        System.out.println(notas100 + " nota(s) de R$ 100,00");
+        System.out.println(notas50 + " nota(s) de R$ 50,00");
+        System.out.println(notas20 + " nota(s) de R$ 20,00");
+        System.out.println(notas10 + " nota(s) de R$ 10,00");
+        System.out.println(notas5 + " nota(s) de R$ 5,00");
+        System.out.println(notas2 + " nota(s) de R$ 2,00");
+        System.out.println(notas1 + " nota(s) de R$ 1,00");
+    }
+
+}
