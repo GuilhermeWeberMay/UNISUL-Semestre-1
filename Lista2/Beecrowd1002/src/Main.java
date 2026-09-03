c1ebc53b689c1fece128492e03706ac439ba27c5
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-09-03 12:58:56 -0300

    Lista2 completa

---
+++ Lista2/Beecrowd1002/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double raio = sc.nextDouble();
+
+        double area = Math.PI * Math.pow(raio, 2);
+
+        System.out.printf("A=%.4f%n", area);
+    }
+}
---
+++ Lista2/Beecrowd1003/src/Main.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int A, B, SOMA;
+        A=input.nextInt();
+        B=input.nextInt();
+        SOMA=A+B;
+        System.out.println("SOMA = "+SOMA);
+        
+    }
+    
+}
