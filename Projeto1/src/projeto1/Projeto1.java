a1952ccea747157fa70aa677394064a28c95c460
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-09-08 20:27:47 -0300

    projeto aula 08.09.26

---
+++ Projeto1/src/projeto1/Projeto1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projeto1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Projeto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c,d, soma;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        soma = a+b+c+d;
+        System.out.println("Soma:"+soma);
+        
+    }
+    
+}
