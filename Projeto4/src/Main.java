e4cebfe4479e1403e4c7b026221b4775503fb78c
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-09-21 19:19:04 -0300

    Projeto4 feito

---
+++ Projeto4/src/Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero = sc.nextInt();
+        
+        if (numero > 10){
+            System.err.println(numero / 2);
+        }
+    }
+    
+}
